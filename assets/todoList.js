5db78ad3497f177b086f4743b364c5024f518381
devendra-alt <devendramulewa171@gmail.com>
2023-06-14 08:02:43 +0530

    Populated todo list

---
+++ assets/todoList.js
@@ -0,0 +1,29 @@
+const toDoTasks = [
+  {
+    description: 'task 1',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'task 2',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'task 3',
+    completed: true,
+    index: 3,
+  },
+  {
+    description: 'task 4',
+    completed: false,
+    index: 4,
+  },
+  {
+    description: 'task 5',
+    completed: false,
+    index: 5,
+  },
+];
+
+export default toDoTasks;
